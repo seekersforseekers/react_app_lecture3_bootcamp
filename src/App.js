20356a79565d3a63af6a5285d7c4c87ea8d7a057
fakhraaslam <fakhraaslam1221@gmail.com>
2020-06-07 23:32:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';  // it is very important react is class React is property or funtion of that class
+/*import React from 'react';  // it is very important react is class React is property or funtion of that class
 function App()
 {
 return <div>Hello world from Fakhra</div>
 }
 export default App; //it is a component defined in node.js
 //
+*/
+import React from 'react';
+import './App.css';
+import Dinner from './Dinner.js';
+function App()
+{
+    return
+    (
+        <div ClassName="App">
+        <Dinner dishName="chikenkraahi" sweetdish="kheer"/>
+        </div>
+    );
+}
+export default App;
