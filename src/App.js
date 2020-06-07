066e854e04e1fab1cc9199772e4517d46ac06a09
fakhraaslam <fakhraaslam1221@gmail.com>
2020-06-07 23:38:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ export default App; //it is a component defined in node.js
 import React from 'react';
 import './App.css';
 import Dinner from './Dinner.js';
-function App()
-{
+function App(){
     return
     (
         <div ClassName="App">
