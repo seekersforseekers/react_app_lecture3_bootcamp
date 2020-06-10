5396a5c057637ae98ff35e9d72c9b4dff25357b9
fakhraaslam <fakhraaslam1221@gmail.com>
2020-06-10 21:06:04 +0500

    Update practise.js

--- src/practise.js
+++ src/practise.js
@@ -2,9 +2,9 @@ import React from 'react';
 function Holder(props){
     return(
         <div>
-<h2> "I m practsing react function"{props.time}/</h2>
-<h3 > "I m practsing react function"{props.name}></h3>
-<h4 > "I m practsing react function"{props.subject}></h4>
+<h2> I m practsing react function"{props.time}</h2>
+<h3 > My Name is"{props.name}</h3>
+<h4 > This is"{props.subject}</h4>
 </div>
     );
 
